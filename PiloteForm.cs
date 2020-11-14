60c16cc66b449885571a6d4a674fbde6c808ca1c
issamBoutissante <boutissante.issam.dev@gmail.com>
2020-11-14 18:41:50 +0100

    the easy metho but long code

--- PiloteForm.cs
+++ PiloteForm.cs
@@ -1,29 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
 namespace Insert_in_Database__MDI_
 {
     public partial class PiloteForm : Form
     {
-        Action<string> Insert_Info;
-        public PiloteForm(Action<string> insert_Info)
+
+        string connection_String = ConfigurationManager.ConnectionStrings["mon_connection"].ConnectionString;
+        SqlConnection connection;
+        SqlCommand command;
+        public PiloteForm()
         {
             InitializeComponent();
-            Insert_Info = insert_Info;
+
         }
         private void Inserer_Pilote_Click(object sender, EventArgs e)
         {
             string query = string.Format("insert into pilote(nom,CodePostal,ville) values('{0}','{1}','{2}')",
                                      Pilote_Nom.Text, Pilote_Code_Postal.Text, Pilote_ville.Text);
-            Insert_Info(query);
-
+            try
+            {
+                using (connection)
+                {
+                    connection = new SqlConnection(connection_String);
+                    command = new SqlCommand(query, connection);
+                    connection.Open();
+                    int nombre_lignes_affecte = command.ExecuteNonQuery();
+                    MessageBox.Show(nombre_lignes_affecte + " lignes a ete affecte.");
+                }
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
